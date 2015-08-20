4ff39763747d6b60fcdae10018af2eda54d84d22
rain-e <314365215@qq.com>
2015-08-20 17:53:00 +0800

    集成框架

---
+++ library_base/src/main/java/com/lemon/library/eventbus/EventCenter.java
@@ -0,0 +1,46 @@
+package com.lemon.library.eventbus;
+
+/**
+ * 作者：lemon
+ * 日期：2015-08-20
+ */
+public class EventCenter<T> {
+
+    /**
+     * reserved data
+     */
+    private T data;
+
+    /**
+     * this code distinguish between different events
+     */
+    private int eventCode = -1;
+
+    public EventCenter(int eventCode) {
+        this(eventCode, null);
+    }
+
+    public EventCenter(int eventCode, T data) {
+        this.eventCode = eventCode;
+        this.data = data;
+    }
+
+    /**
+     * get event code
+     *
+     * @return
+     */
+    public int getEventCode() {
+        return this.eventCode;
+    }
+
+    /**
+     * get event reserved data
+     *
+     * @return
+     */
+    public T getData() {
+        return this.data;
+    }
+}
+
